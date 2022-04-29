5ea91292250ca28e22c939a135d7f2aad3f12606
AbrarSL <abrarsl2002@gmail.com>
2022-04-29 18:09:08 +0530

    Add helper functions to create a div grid and also clear it

---
+++ script.js
@@ -0,0 +1,41 @@
+const gridTotalSize = 960;
+
+const gridSizeInput = document.querySelector('#gridSizeInput');
+gridSizeInput.addEventListener('change', updateGridSize);
+
+function updateGridSize(event) {
+  const gridSizeInput = event.target;
+  const gridSizeLabel = gridSizeInput.previousElementSibling;
+  const drawingGrid = document.querySelector('.drawing-grid');
+
+  gridSizeLabel.textContent = `${gridSizeInput.value}×${gridSizeInput.value}`;
+
+  removeAllChildElements(drawingGrid);
+  generateDivGrid(drawingGrid, gridSizeInput.value);
+}
+
+function generateDivGrid(parentElement, sideLength) {
+  for (let i = 0; i < sideLength; i++) {
+    const pixelRow = document.createElement('div');
+
+    pixelRow.classList.add('pixel-row');
+
+    for (let i = 0; i < sideLength; i++) {
+      const pixel = document.createElement('div');
+
+      pixel.classList.add('pixel');
+      pixel.style.width = (gridTotalSize / sideLength) + 'px';
+      pixel.style.height = pixel.style.width;
+
+      pixelRow.appendChild(pixel);
+    }
+
+    parentElement.appendChild(pixelRow);
+  }
+}
+
+function removeAllChildElements(parentElement) {
+  while (parentElement.lastElementChild) {
+    parentElement.removeChild(parentElement.lastElementChild);
+  }
+}
